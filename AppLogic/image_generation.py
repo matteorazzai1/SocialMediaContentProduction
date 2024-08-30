a5165001843b54beaa0f5d93188d04256f160fff
matteorazzai1 <118298555+matteorazzai1@users.noreply.github.com>
2024-08-30 17:23:43 +0200

    small fixes in name and path in order to retrieve the image generated

--- AppLogic/image_generation.py
+++ AppLogic/image_generation.py
@@ -1,5 +1,11 @@
 from diffusers import StableDiffusionPipeline
 import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from PIL import Image
+from io import BytesIO
+import requests
+
+
 
 
 def generate_image_example():
@@ -7,21 +13,23 @@ def generate_image_example():
     return image_url
 
 
-def generate_image(self, caption, field, firm):
+def generate_image(caption, field, firm):
     # Load the pre-trained Stable Diffusion model
     model_id = "CompVis/stable-diffusion-v1-4"  # or another model variant
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to("cuda")  # Use GPU if available, otherwise use "cpu"
-    self.update_progress_bar(60)
+    #self.update_progress_bar(60)
 
     # Define a prompt
     prompt = "An image related to this caption" + caption
 
     # Generate an image
     image = pipe(prompt).images[0]
-    self.update_progress_bar(80)
+    #self.update_progress_bar(80)
 
     # Save the generated image
-    image.save("../generated_image/generated_image_" + firm + "_" + field + ".png")
+    URL = "static/images/generated_image_" + firm + "_" + field + ".png"
+    image.save(URL)
 
-    return image
+    return URL
+
